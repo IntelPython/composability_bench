de28675596783d4ee19d379c8509d977f032edb7
Anton Malakhov <Anton.A.Malakhov@gmail.com>
2022-01-28 16:47:55 +0300

    fixed url

--- set_python_envs.sh
+++ set_python_envs.sh
@@ -28,7 +28,7 @@ DIR=$HOME/miniconda3
 rm -r $DIR
 mkdir -p $DIR
 cd $DIR
-curl -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
+curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
     && bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $DIR -f \
     && rm ./Miniconda3-latest-Linux-x86_64.sh
 #export ACCEPT_INTEL_PYTHON_EULA=yes
